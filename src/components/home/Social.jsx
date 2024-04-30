8f1a0701b1f440839910a1999f43843bd8a453b6
AlexisJhar <alexiissalexander@gmail.com>
2024-04-30 15:19:07 +0200

    portfolio

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Social = () => {
+    return (
+        <div className="home__social">
+            <a href="https://www.instagram.com/" className="home__social-icon" target="_blank" rel="noreferrer">
+                <i className="uil uil-instagram"></i>
+            </a>
+
+            <a href="https://www.linkedin.com/in/g-alexander-carri%C3%B3n-79b785296/" className="home__social-icon" target="_blank" rel="noreferrer">
+                <i class="uil uil-linkedin-alt"></i>
+            </a>
+
+            <a href="https://github.com/AlexisJhar" className="home__social-icon" target="_blank" rel="noreferrer">
+                <i className="uil uil-github-alt"></i>
+            </a>
+        </div>
+    )
+}
+
+export default Social
